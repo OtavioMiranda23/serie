e5a04114a254f19db597ae86eaca430792985e92
OtavioMiranda23 <otaviomiranda@usp.br>
2023-11-20 15:08:46 -0300

    adiciona carrosel brands

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header";
+import BrandBar from "./components/brandBar";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Adiciona o ouvinte de evento para resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove o ouvinte de evento ao desmontar o componente
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <Header />
-
-  )
-
-
-
-  
+    <>
+      <Header />
+      <section>
+        <div className="banner-full-container">
+          <img
+            src={`${isMobile ? "./src/assets/BannerFull-mobile.png" : "./src/assets/BannerFull.png"}`}
+            alt="Banner principal"
+          />
+        </div>
+      </section>
+      <BrandBar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/brandBar/index.tsx
@@ -0,0 +1,47 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./styles.css";
+import "swiper/swiper-bundle.css";
+
+export default function BrandBar() {
+  return (
+    <section className="brandbar">
+      <div className="brandbar-content section">
+        <Swiper
+          direction="horizontal"
+          slidesPerView={5}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          breakpoints={{
+            768: {
+              width: 768,
+              slidesPerView: 5,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <img src="./src/assets/BrandAOC.png" alt="Logo Aoc" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandClearBrazil.png" alt="Logo Clear" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandGamerscard.png" alt="Logo Gamerscard" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandHyperX.png" alt="Logo HyperX" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandLegion.png" alt="Logo Legion" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandNike.png" alt="Logo Nike" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./src/assets/BrandRivalry.png" alt="Logo Rivalry" />
+          </SwiperSlide>
+          ...
+        </Swiper>
+      </div>
+    </section>
+  );
+}
